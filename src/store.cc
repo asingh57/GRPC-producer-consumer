0e83372bdde99c7e5b70197756613b4d3a570548
Abhijit Singh <abhi_johnian@hotmail.com>
2020-11-01 14:52:09 -0500

    added sleep and restructured definitions

--- src/store.cc
+++ src/store.cc
@@ -1,31 +1,8 @@
 
 #include "store.h"
 
-using grpc::Channel;
-using grpc::ClientAsyncResponseReader;
-using grpc::ClientContext;
-using grpc::CompletionQueue;
-using grpc::Status;
-using vendor::BidQuery;
-using vendor::BidReply;
-using vendor::Vendor;
-using std::cout;
-using std::endl;
 
-typedef struct bidFinishInfo{
-    std::shared_ptr<ClientAsyncResponseReader<BidReply>> rpc;
-    std::shared_ptr<CompletionQueue> cq;
-    std::shared_ptr<ClientContext> ctx;
-    std::shared_ptr<BidReply> vr;
-    std::shared_ptr<Status> st;
-} BidFinishInfo;
-
-class VendorClient {
- public:
-  explicit VendorClient(std::shared_ptr<Channel> channel)
-      : stub_(Vendor::NewStub(channel)) {}
-
-   std::shared_ptr<BidFinishInfo> getProductBid(const std::string& product) {
+std::shared_ptr<BidFinishInfo> VendorClientChannel::getProductBid(const std::string& product) {
     BidQuery request;
     request.set_product_name(product);
 
@@ -35,14 +12,10 @@ class VendorClient {
     std::shared_ptr<ClientAsyncResponseReader<BidReply> > rpc(
         stub_->AsyncgetProductBid(ctx.get(), request, cq.get()));
 
-    
-
     std::shared_ptr<BidReply> vr=  std::shared_ptr<BidReply>(new BidReply);
     std::shared_ptr<Status> st=  std::shared_ptr<Status>(new Status);
 
     rpc->Finish(vr.get(), st.get(), (void*)1);
-
-
     std::shared_ptr<BidFinishInfo> finInf=std::shared_ptr<BidFinishInfo>(new BidFinishInfo);
 
     finInf->vr=vr;
@@ -50,54 +23,23 @@ class VendorClient {
     finInf->cq=cq;
     finInf->rpc=rpc;
     finInf->ctx=ctx;
-    
-    
     return finInf;
-  }
-    std::shared_ptr<BidReply> getVendorResult(std::shared_ptr<BidFinishInfo> bfi){ 
-        void* got_tag;
-        bool ok = false;
-        //block here
-        GPR_ASSERT(bfi->cq->Next(&got_tag, &ok));
-        GPR_ASSERT(got_tag == (void*)1);
-        GPR_ASSERT(ok);
-        if (bfi->st->ok()) {
-        } else {
-            cout<< "error, getProductBid failed";
-        }
-        return bfi->vr;
+}
+std::shared_ptr<BidReply> VendorClientChannel::getVendorResult(std::shared_ptr<BidFinishInfo> bfi){ 
+    void* got_tag;
+    bool ok = false;
+    //block here
+    GPR_ASSERT(bfi->cq->Next(&got_tag, &ok));
+    GPR_ASSERT(got_tag == (void*)1);
+    GPR_ASSERT(ok);
+    if (bfi->st->ok()) {
+    } else {
+        cout<< "error, getProductBid failed";
     }
-
- private:
-  std::unique_ptr<Vendor::Stub> stub_;
-};
+    return bfi->vr;
+}
 
 
-
-
-
-typedef struct blockingThreadpoolVendorCallerData{
-    mutex my_thread_mutex;//caller can block on this to get status of job
-    VendorClient *myVendorChannel;
-    std::shared_ptr<BidFinishInfo> myBidFinishInfo;
-    std::shared_ptr<BidReply> myVendorReply;   
-    string productName;
-    std::shared_ptr<Threadpool> tp_;
-    blockingThreadpoolVendorCallerData(
-            VendorClient *vc,
-            string prod,            
-            std::shared_ptr<Threadpool> tp
-        )
-        :        
-        myVendorChannel(vc),
-        productName(prod),
-        tp_(tp),
-        my_thread_mutex()    
-    {
-        cout <<"thread callerdata locked"<<endl;
-        my_thread_mutex.lock();//lock to indicate we job is created
-    }
-}blockingThreadpoolVendorCallerData;
 
 
 //callback for threadpool to finish receiving data
@@ -139,19 +81,18 @@ void lockingThreadpoolVendorCallStartRequest(void* data){
 
 
 
-class ServerImpl final {
- public:
-  ~ServerImpl() {
+
+  ServerImpl::~ServerImpl() {
     server_->Shutdown();
     cq_->Shutdown();
   }
 
-  ServerImpl(std::shared_ptr<Threadpool> tp ,vector<VendorClient*> vcList){
+  ServerImpl::ServerImpl(std::shared_ptr<Threadpool> tp ,vector<VendorClientChannel*> vcList){
      tp_=tp;
     vcList_=vcList;
     }
 
-  void Run(int port) {
+  void ServerImpl::Run(int port) {
     std::string server_address("0.0.0.0:"+to_string(port));
     ServerBuilder builder;
     builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
@@ -161,16 +102,26 @@ class ServerImpl final {
     std::cout << "Server listening on " << server_address << std::endl;
     HandleRpcs();
   }
- private:
-    std::shared_ptr<Threadpool> tp_;
-    vector<VendorClient*> vcList_;
-  class StoreCallData {
-   public:
-    StoreCallData(Store::AsyncService* service, ServerCompletionQueue* cq,vector<VendorClient*> vcList, std::shared_ptr<Threadpool> tpList)
+ 
+
+
+    ServerImpl::StoreCallData::StoreCallData(Store::AsyncService* service, ServerCompletionQueue* cq,vector<VendorClientChannel*> vcList, std::shared_ptr<Threadpool> tpList)
         : service_(service), cq_(cq), responder_(&ctx_), status_(CREATE),vcList_(vcList),tp_(tpList) {
       Proceed();
     }
-    void Proceed() {
+
+  void ServerImpl::HandleRpcs() {
+    new StoreCallData(&service_, cq_.get(),vcList_,tp_);
+    void* tag; 
+    bool ok;
+    while (true) {
+      GPR_ASSERT(cq_->Next(&tag, &ok));
+      GPR_ASSERT(ok);
+      static_cast<StoreCallData*>(tag)->Proceed();
+    }
+  }
+
+    void ServerImpl::StoreCallData::Proceed() {
       if (status_ == CREATE) {
         
         cout <<"creating"<<endl;
@@ -226,9 +177,6 @@ class ServerImpl final {
         pd->set_price(myPoolReq[i]->myVendorReply->price());
         pd->set_vendor_id(myPoolReq[i]->myVendorReply->vendor_id());
 
-        //TODO modify reply_
-        //reply_.set_message(prefix + request_.name());
-
         }
 
         
@@ -240,32 +188,6 @@ class ServerImpl final {
         delete this;
       }
     }
-   private:    
-    vector<VendorClient*> vcList_;
-    std::shared_ptr<Threadpool> tp_;
-    Store::AsyncService* service_;
-    ServerCompletionQueue* cq_;
-    ServerContext ctx_;
-    ProductQuery request_;
-    ProductReply reply_;
-    ServerAsyncResponseWriter<ProductReply> responder_;
-    enum CallStatus { CREATE, PROCESS, FINISH };
-    CallStatus status_;
-  };
-  void HandleRpcs() {
-    new StoreCallData(&service_, cq_.get(),vcList_,tp_);
-    void* tag; 
-    bool ok;
-    while (true) {
-      GPR_ASSERT(cq_->Next(&tag, &ok));
-      GPR_ASSERT(ok);
-      static_cast<StoreCallData*>(tag)->Proceed();
-    }
-  }
-  std::unique_ptr<ServerCompletionQueue> cq_;
-  Store::AsyncService service_;
-  std::unique_ptr<Server> server_;
-};
 
 
 Threadpool::Threadpool(int threadCt):m_shutdown(false),m_jbs(),m_pool(threadCt),m_job_mutex(){
@@ -285,6 +207,9 @@ void Threadpool::infiniteLooper(Threadpool *tp){
             tp->m_job_mutex.unlock();
             if(myJob){
                 myJob->action(myJob->data);
+            }
+            else{
+                sleep(0.1);
             }
         }
 }
@@ -328,13 +253,13 @@ int main(int argc, char** argv) {
     std::ifstream infile(fileName);
 
 
-    vector<VendorClient*> VendorClientList;
+    vector<VendorClientChannel*> VendorClientChannelList;
     if (infile.is_open())
       {
       string line;
         while ( getline (infile,line) )
         {
-            VendorClientList.push_back(new VendorClient(grpc::CreateChannel(
+            VendorClientChannelList.push_back(new VendorClientChannel(grpc::CreateChannel(
       line, grpc::InsecureChannelCredentials())));
         }
         infile.close();
@@ -343,7 +268,7 @@ int main(int argc, char** argv) {
     
     std::shared_ptr<Threadpool> tp(new Threadpool(threadCt));
 
-    ServerImpl server(tp,VendorClientList);
+    ServerImpl server(tp,VendorClientChannelList);
     server.Run(port);                                                       
 
   return 0;
--- src/store.h
+++ src/store.h
@@ -11,6 +11,8 @@
 #include "vendor.pb.h"
 #include "store.grpc.pb.h"
 #include "store.pb.h"
+#include <unistd.h>
+
 #include <iostream>
 
 using std::to_string;
@@ -26,3 +28,111 @@ using store::ProductReply;
 using store::ProductInfo;
 using store::Store;
 using std::string;
+using grpc::Channel;
+using grpc::ClientAsyncResponseReader;
+using grpc::ClientContext;
+using grpc::CompletionQueue;
+using grpc::Status;
+using vendor::BidQuery;
+using vendor::BidReply;
+using vendor::Vendor;
+using std::cout;
+using std::endl;
+
+
+typedef struct bidFinishInfo{
+    std::shared_ptr<ClientAsyncResponseReader<BidReply>> rpc;
+    std::shared_ptr<CompletionQueue> cq;
+    std::shared_ptr<ClientContext> ctx;
+    std::shared_ptr<BidReply> vr;
+    std::shared_ptr<Status> st;
+} BidFinishInfo;
+
+
+
+
+
+class VendorClientChannel {
+ public:
+  explicit VendorClientChannel(std::shared_ptr<Channel> channel)
+      : stub_(Vendor::NewStub(channel)) {}
+
+   std::shared_ptr<BidFinishInfo> getProductBid(const std::string& product);
+
+    std::shared_ptr<BidReply> getVendorResult(std::shared_ptr<BidFinishInfo> bfi);
+
+ private:
+  std::unique_ptr<Vendor::Stub> stub_;
+};
+
+
+typedef struct blockingThreadpoolVendorCallerData{
+    mutex my_thread_mutex;//caller can block on this to get status of job
+    VendorClientChannel *myVendorChannel;
+    std::shared_ptr<BidFinishInfo> myBidFinishInfo;
+    std::shared_ptr<BidReply> myVendorReply;   
+    string productName;
+    std::shared_ptr<Threadpool> tp_;
+    blockingThreadpoolVendorCallerData(
+            VendorClientChannel *vc,
+            string prod,            
+            std::shared_ptr<Threadpool> tp
+        )
+        :        
+        myVendorChannel(vc),
+        productName(prod),
+        tp_(tp),
+        my_thread_mutex()    
+    {
+        cout <<"thread callerdata locked"<<endl;
+        my_thread_mutex.lock();//lock to indicate we job is created
+    }
+}blockingThreadpoolVendorCallerData;
+
+
+
+
+
+//callback for threadpool to finish receiving data
+void lockingThreadpoolVendorCallFinishRequest(void* data);
+
+
+//callback for threadpool that calls into the assigned vendor 
+void lockingThreadpoolVendorCallStartRequest(void* data);
+
+
+
+class ServerImpl final {
+ public:
+  ~ServerImpl();
+
+  ServerImpl(std::shared_ptr<Threadpool> tp ,vector<VendorClientChannel*> vcList);
+
+  void Run(int port);
+ private:
+    std::shared_ptr<Threadpool> tp_;
+    vector<VendorClientChannel*> vcList_;
+  class StoreCallData {
+   public:
+    StoreCallData(Store::AsyncService* service, ServerCompletionQueue* cq,vector<VendorClientChannel*> vcList, std::shared_ptr<Threadpool> tpList);
+        
+    void Proceed();
+   private:    
+    vector<VendorClientChannel*> vcList_;
+    std::shared_ptr<Threadpool> tp_;
+    Store::AsyncService* service_;
+    ServerCompletionQueue* cq_;
+    ServerContext ctx_;
+    ProductQuery request_;
+    ProductReply reply_;
+    ServerAsyncResponseWriter<ProductReply> responder_;
+    enum CallStatus { CREATE, PROCESS, FINISH };
+    CallStatus status_;
+  };
+  void HandleRpcs();
+  std::unique_ptr<ServerCompletionQueue> cq_;
+  Store::AsyncService service_;
+  std::unique_ptr<Server> server_;
+};
+
+
